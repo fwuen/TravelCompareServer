959e79d9329c7a143284f4ba80b20c4275830e24
fwuen <git@fwuen.de>
2017-11-06 14:26:01 +0100

    Kleinigkeiten

--- src/main/java/travelcompare/restapi/external/lufthansa/model/ScheduleResource.java
+++ src/main/java/travelcompare/restapi/external/lufthansa/model/ScheduleResource.java
@@ -2,6 +2,8 @@ package travelcompare.restapi.external.lufthansa.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import java.util.List;
+
 public class ScheduleResource {
     @JsonProperty(value = "Schedule")
     private List<Schedule> schedule;
