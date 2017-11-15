4e79caf0fa0e1a7e250a4dc9fb4497d5232c69ef
fwuen <git@fwuen.de>
2017-11-15 21:31:30 +0100

    Lufthansa Authentifzierung umgestellt (geht noch nicht)

--- src/main/java/travelcompare/restapi/external/lufthansa/LufthansaConsumer.java
+++ src/main/java/travelcompare/restapi/external/lufthansa/LufthansaConsumer.java
@@ -3,7 +3,6 @@ package travelcompare.restapi.external.lufthansa;
 import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.exceptions.UnirestException;
 import travelcompare.restapi.external.Consumer;
-import travelcompare.restapi.external.lufthansa.model.FaresResponse;
 import travelcompare.restapi.external.lufthansa.response.*;
 
 import java.util.Calendar;
@@ -13,14 +12,19 @@ public class LufthansaConsumer extends Consumer {
 
     public static final String clientId = "utj2w984ec8utsps5jky9fxp";
     public static final String clientSecret = "bX9kw4CpG6";
-    private String accessToken;
     private String tokenType = "Bearer";
-    private Date lastAuthenticated;
-    private long expiresIn;
+    private Date publicLastAuthenticated;
+    private long publicExpiresIn;
+    private String publicAccessToken;
+    
+    private Date partnerLastAuthenticated;
+    private long partnerExpiresIn;
+    private String partnerAccessToken;
 
 
     public LufthansaConsumer() {
-        authenticate();
+        authenticate(false);
+        authenticate(true);
     }
 
     @Override
@@ -29,33 +33,33 @@ public class LufthansaConsumer extends Consumer {
     }
 
     public FlightStatusResponse consumeFlightStatus(String flightNumber, String date) throws UnirestException {
-        if(!stillAuthenticated()) authenticate();
+        if(!publicStillAuthenticated()) authenticate(false);
         return Unirest.get(getBaseURL() + "operations/flightstatus/" + flightNumber + "/" + date).
-                header("Authorization", tokenType + " " + accessToken).
+                header("Authorization", tokenType + " " + publicAccessToken).
                 asObject(FlightStatusResponse.class).
                 getBody();
     }
 
     public FlightScheduleResponse consumeFlightSchedule(String origin, String destination, String date) throws UnirestException {
-        if(!stillAuthenticated()) authenticate();
+        if(!publicStillAuthenticated()) authenticate(false);
         return Unirest.get(getBaseURL() + "operations/schedules/" + origin + "/" + destination + "/" + date).
-                header("Authorization", tokenType + " " + accessToken).
+                header("Authorization", tokenType + " " + publicAccessToken).
                 asObject(FlightScheduleResponse.class).
                 getBody();
     }
 
     public NearestAirportResponse consumeNearestAirport(String lat, String lon) throws UnirestException {
-        if(!stillAuthenticated()) authenticate();
+        if(!publicStillAuthenticated()) authenticate(false);
         return Unirest.get(getBaseURL() + "references/airports/nearest/" + lat + "," + lon).
-                header("Authorization", tokenType + " " + accessToken).
+                header("Authorization", tokenType + " " + publicAccessToken).
                 asObject(NearestAirportResponse.class).
                 getBody();
     }
     
     public AllFaresResponse consumeAllFares(String catalogues, String origin, String destination, String travelDate) throws UnirestException {
-        if(!stillAuthenticated()) authenticate();
+        if(!partnerStillAuthenticated()) authenticate(true);
         return Unirest.get(getBaseURL() + "offers/fares/allfares").
-                header("Authorization", tokenType + " " + "h5k24v9u58e2sgpr99jusbrc").
+                header("Authorization", tokenType + " " + partnerAccessToken).
                 queryString("catalogues", catalogues).
                 queryString("origin", origin).
                 queryString("destination", destination).
@@ -64,33 +68,56 @@ public class LufthansaConsumer extends Consumer {
                 getBody();
     }
 
-    private void authenticate() {
+    private void authenticate(boolean isPartnerRequest) {
         AuthenticationResponse response =
                 null;
-        try {
-            response = Unirest.post(getBaseURL() + "oauth/token").
-                    field("client_id", clientId).
-                    field("client_Secret", clientSecret).
-                    field("grant_type", "client_credentials").
-                    asObject(AuthenticationResponse.class).
-                    getBody();
-        } catch (UnirestException e) {
-            e.printStackTrace();
+        if(isPartnerRequest) {
+            try {
+                response = Unirest.post(getBaseURL() + "partners/oauth/token").
+                        field("client_id", clientId).
+                        field("client_Secret", clientSecret).
+                        field("grant_type", "client_credentials").
+                        asObject(AuthenticationResponse.class).
+                        getBody();
+            } catch (UnirestException e) {
+                e.printStackTrace();
+            }
+            publicExpiresIn = response.getExpiresIn();
+            publicLastAuthenticated = response.getLastAuthenticated();
+            publicAccessToken = response.getAccessToken();
+        } else {
+            try {
+                response = Unirest.post(getBaseURL() + "oauth/token").
+                        field("client_id", clientId).
+                        field("client_Secret", clientSecret).
+                        field("grant_type", "client_credentials").
+                        asObject(AuthenticationResponse.class).
+                        getBody();
+            } catch (UnirestException e) {
+                e.printStackTrace();
+            }
+            partnerExpiresIn = response.getExpiresIn();
+            partnerLastAuthenticated = response.getLastAuthenticated();
+            partnerAccessToken = response.getAccessToken();
         }
-
-        lastAuthenticated = response.getLastAuthenticated();
-        accessToken = response.getAccessToken();
-        expiresIn = response.getExpiresIn();
+        
     }
-
     private long getTimestampFromDate(Date date) {
         Calendar c = Calendar.getInstance();
         c.setTime(date);
         return c.getTimeInMillis();
     }
 
-    private boolean stillAuthenticated() {
-        if(getTimestampFromDate(lastAuthenticated) + expiresIn < getTimestampFromDate(new Date())) {
+    private boolean publicStillAuthenticated() {
+        if(getTimestampFromDate(publicLastAuthenticated) + publicExpiresIn < getTimestampFromDate(new Date())) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    
+    private boolean partnerStillAuthenticated() {
+        if(getTimestampFromDate(partnerLastAuthenticated) + partnerExpiresIn < getTimestampFromDate(new Date())) {
             return true;
         } else {
             return false;
