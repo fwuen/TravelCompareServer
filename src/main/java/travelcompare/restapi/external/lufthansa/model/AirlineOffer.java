2697f1657c1338d01bf7daa5c8a5fda44bdcf854
fwuen <git@fwuen.de>
2017-11-15 22:46:29 +0100

    Fix

--- src/main/java/travelcompare/restapi/external/lufthansa/model/AirlineOffer.java
+++ src/main/java/travelcompare/restapi/external/lufthansa/model/AirlineOffer.java
@@ -1,10 +1,14 @@
 package travelcompare.restapi.external.lufthansa.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.List;
 
 public class AirlineOffer {
     @JsonProperty(value = "OfferID")
-    private OfferId offerId;
+    @JsonFormat(with = JsonFormat.Feature.ACCEPT_SINGLE_VALUE_AS_ARRAY)
+    private List<OfferId> offerId;
     
     @JsonProperty(value = "TotalPrice")
     private TotalPrice totalPrice;
