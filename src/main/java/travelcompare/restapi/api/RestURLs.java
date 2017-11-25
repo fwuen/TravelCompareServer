35736727b50fbe760683f533f2cb025692f38916
Dominic Fuchs <syntarex@gmail.com>
2017-11-25 19:44:50 +0100

    Login angepasst.

---
+++ src/main/java/travelcompare/restapi/api/RestURLs.java
@@ -0,0 +1,7 @@
+package travelcompare.restapi.api;
+
+public class RestURLs {
+
+    public static final String REGISTER = "/register";
+
+}
--- src/main/java/travelcompare/restapi/provider/TrainRouteProvider.java
+++ src/main/java/travelcompare/restapi/provider/TrainRouteProvider.java
@@ -4,6 +4,7 @@ import travelcompare.restapi.provider.model.Route;
 import travelcompare.restapi.provider.model.TrainStation;
 
 public class TrainRouteProvider implements RouteProvider<TrainStation> {
+
     @Override
     public Route getRoute(TrainStation start, TrainStation destination) {
         return null;
