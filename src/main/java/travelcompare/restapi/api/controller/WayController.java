10317b1235f3d3bf8d401d4ca36e5490ef64537b
fwuen <git@fwuen.de>
2017-12-17 17:32:44 +0100

    folgende Way-Schnittstellen implementiert: - post - get - put

--- src/main/java/travelcompare/restapi/api/controller/WayController.java
+++ src/main/java/travelcompare/restapi/api/controller/WayController.java
@@ -2,13 +2,16 @@ package travelcompare.restapi.api.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import travelcompare.restapi.api.RestURLs;
 import travelcompare.restapi.api.model.request.WayData;
+import travelcompare.restapi.data.model.User;
 import travelcompare.restapi.data.model.Way;
+import travelcompare.restapi.data.service.UserService;
 import travelcompare.restapi.data.service.WayService;
+
+import java.security.Principal;
+import java.util.Optional;
 
 @RestController
 public class WayController {
@@ -16,12 +19,57 @@ public class WayController {
     @Autowired
     private WayService wayService;
 
+    @Autowired
+    private UserService userService;
+
     @PostMapping(RestURLs.WAY_POST)
     public ResponseEntity<Way> post(
-            @RequestBody WayData data
+            @RequestBody WayData data,
+            Principal principal
     ) {
+        if(!data.valid().isValid())
+            return ResponseEntity.status(400).build();
 
-        return null;
+        long creatorId;
+        if(userService.getUserByEmail(principal.getName()).isPresent()) {
+            creatorId = userService.getUserByEmail(principal.getName()).get().getId();
+        } else {
+            return ResponseEntity.status(401).build();
+        }
+
+        data.setCreatorId(creatorId);
+        Way way = wayService.createRoute(data);
+
+        return ResponseEntity.ok(way);
+    }
+
+    @PutMapping(RestURLs.WAY_PUT)
+    public ResponseEntity<Way> put(
+            @RequestBody Way way,
+            Principal principal
+    ) {
+        Optional<User> loggedInUser = userService.getUserByEmail(principal.getName());
+
+        if(!loggedInUser.isPresent() || loggedInUser.get().getId() != way.getCreatorId())
+            return ResponseEntity.status(403).build();
+
+        return ResponseEntity.ok(
+                wayService.updateRoute(way)
+        );
+    }
+
+    @GetMapping(RestURLs.WAY_GET)
+    public ResponseEntity<Way> get(
+            @PathVariable("id") long id
+    ) {
+        Optional<Way> wayOptional = wayService.getWayById(id);
+
+        if(!wayOptional.isPresent())
+            return ResponseEntity.status(404).build();
+
+        return ResponseEntity.ok(
+                wayOptional.get()
+        );
     }
 
 }
