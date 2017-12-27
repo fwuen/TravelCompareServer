c23e2fd38aa85a362ff9834673ecc63474b78b96
Marco <marcogeissler69@gmail.com>
2017-12-27 15:39:57 +0100

    find

--- src/main/java/travelcompare/restapi/api/controller/WayController.java
+++ src/main/java/travelcompare/restapi/api/controller/WayController.java
@@ -2,15 +2,24 @@ package travelcompare.restapi.api.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.method.P;
 import org.springframework.web.bind.annotation.*;
 import travelcompare.restapi.api.RestURLs;
+import travelcompare.restapi.api.model.request.WAY_TYPE;
 import travelcompare.restapi.api.model.request.WayData;
 import travelcompare.restapi.data.model.User;
 import travelcompare.restapi.data.model.Way;
 import travelcompare.restapi.data.service.UserService;
 import travelcompare.restapi.data.service.WayService;
+import travelcompare.restapi.external.tankerkoenig.response.FUEL_TYPE;
+import travelcompare.restapi.logic.CheapestWayProvider;
+import travelcompare.restapi.logic.FastestWayProvider;
+import travelcompare.restapi.provider.model.Geo;
 
+import javax.xml.ws.Response;
 import java.security.Principal;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Optional;
 
 //TODO: nochmal über Statuscodes schauen
@@ -100,4 +109,64 @@ public class WayController {
 
     }
 
+    @GetMapping(RestURLs.WAY_FIND)
+    public ResponseEntity<travelcompare.restapi.provider.model.Way> find(
+            @PathVariable("start_lat") double start_lat,
+            @PathVariable("start_lon") double start_lon,
+            @PathVariable("dest_lat") double dest_lat,
+            @PathVariable("dest_lon") double dest_lon,
+            @PathVariable("date") String date,
+            @PathVariable(value = "radius", required = false) int radius,
+            @PathVariable(value = "fuel_type", required = false) String fuel_type,
+            @PathVariable(value = "way_category", required = false) String way_category,
+            Principal principal
+    ) {
+
+        CheapestWayProvider cheapestWayProvider = new CheapestWayProvider();
+        FastestWayProvider fastestWayProvider = new FastestWayProvider();
+
+        if(!userService.userExistsByEmail(principal.getName()))
+            return ResponseEntity.status(401).build();
+
+        Optional<User> loggedInUser = userService.getUserByEmail(principal.getName());
+
+        Geo start = new Geo(start_lat, start_lon);
+        Geo dest = new Geo(dest_lat, dest_lon);
+
+        FUEL_TYPE fuelType = null;
+        WAY_TYPE wayType = null;
+
+        if (fuel_type == null) {
+            fuelType = FUEL_TYPE.all;
+        } else {
+            try {
+                fuelType = FUEL_TYPE.valueOf(fuel_type);
+            } catch (IllegalArgumentException e) {
+                return ResponseEntity.status(400).build();
+            }
+        }
+
+        if (way_category == null) {
+            wayType = WAY_TYPE.FASTEST;
+        } else {
+            try {
+                wayType = WAY_TYPE.valueOf(way_category);
+            } catch (IllegalArgumentException e) {
+                return ResponseEntity.status(400).build();
+            }
+        }
+
+        Date formatted_date = new Date(date);
+
+        Optional<travelcompare.restapi.provider.model.Way> returnWay = null;
+        if (wayType.equals(WAY_TYPE.CHEAPEST)) {
+            try {
+                returnWay = cheapestWayProvider.find(start, dest, radius, formatted_date, fuelType);
+            } catch (Exception e) {
+                return ResponseEntity.status(500).build();
+            }
+        }
+
+        return ResponseEntity.ok(returnWay.get());
+    }
 }
--- src/main/java/travelcompare/restapi/external/tankerkoenig/response/FUEL_TYPE.java
+++ src/main/java/travelcompare/restapi/external/tankerkoenig/response/FUEL_TYPE.java
@@ -1,5 +1,15 @@
 package travelcompare.restapi.external.tankerkoenig.response;
 
 public enum FUEL_TYPE {
-    e5, e10, diesel, all
+    e5("e5"), e10("e10"), diesel("diesel"), all("all");
+
+    private String type;
+
+    FUEL_TYPE(String type) {
+        this.type = type;
+    }
+
+    public String type() {
+        return type;
+    }
 }
