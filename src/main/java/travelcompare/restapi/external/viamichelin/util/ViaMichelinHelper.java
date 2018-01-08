f6e5414818c6099571cc1c5ef7e838263dff30ef
Markus <markus.goller@hof-university.de>
2018-01-08 19:18:46 +0100

    ViaMichelinHelper abgespeckt

--- src/main/java/travelcompare/restapi/external/viamichelin/util/ViaMichelinHelper.java
+++ src/main/java/travelcompare/restapi/external/viamichelin/util/ViaMichelinHelper.java
@@ -23,18 +23,6 @@ public class ViaMichelinHelper {
         }
         JSONObject itiJSON = jsonObject.getJSONObject("iti");
 
-        if (!itiJSON.has("ititraceLevels") || itiJSON.getString("ititraceLevels") == null) {
-            return null;
-        }
-        routeIti.setItitraceLevels(itiJSON.getString("ititraceLevels"));
-
-
-        if (!itiJSON.has("itineraryTrace") || itiJSON.getString("itineraryTrace") == null) {
-            return null;
-        }
-        routeIti.setItineraryTrace(itiJSON.getString("itineraryTrace"));
-
-
         if (!itiJSON.has("roadSheet") || itiJSON.getJSONObject("roadSheet") == null || this.parseRoadSheet(itiJSON.getJSONObject("roadSheet")) == null) {
             return null;
         }
@@ -88,31 +76,8 @@ public class ViaMichelinHelper {
             }
             step.setInstructions(item.getString("instructions"));
 
-            if (item.has("mapDef")) {
-                if (item.has("mapDef") && item.getJSONObject("mapDef") != null) {
-                    MapDef mapDef = this.parseMapDef(item.getJSONObject("mapDef"));
-                    step.setMapDef(mapDef);
-                }
-
-            }
             if (item.has("distance")) {
                 step.setDistance(item.getDouble("distance"));
-            }
-
-            if (item.has("level")) {
-                step.setLevel(item.getLong("level"));
-            }
-
-            if (item.has("partialDuration")) {
-                step.setPartialDuration(item.getLong("partialDuration"));
-            }
-
-            if (item.has("gathering")) {
-                step.setGathering(item.getLong("gathering"));
-            }
-
-            if (item.has("partialDistance")) {
-                step.setPartialDistance(item.getDouble("partialDistance"));
             }
 
             if (!item.has("coords") || item.getJSONObject("coords") == null) {
@@ -132,9 +97,6 @@ public class ViaMichelinHelper {
             routeCoords.setLong_coordinate(routeCoordsJSON.getDouble("lon"));
             step.setCoords(routeCoords);
 
-            if (item.has("picto") && item.getString("picto") != null) {
-                step.setPicto(item.getString("picto"));
-            }
             roadSheetStep.add(step);
         }
         roadSheet.setRoadSheetStep(roadSheetStep);
@@ -149,37 +111,6 @@ public class ViaMichelinHelper {
     private RouteHeader parseRouteHeader(JSONObject jsonObject) {
         RouteHeader header = new RouteHeader();
 
-        if (jsonObject.has("startMapDef") && this.parseMapDef(jsonObject.getJSONObject("startMapDef")) != null) {
-            MapDef startMapDef = this.parseMapDef(jsonObject.getJSONObject("startMapDef"));
-            header.setStartMapDef(startMapDef);
-        }
-
-
-        if (jsonObject.has("startMapDef") && this.parseMapDef(jsonObject.getJSONObject("startMapDef")) != null) {
-            MapDef destMapDef = this.parseMapDef(jsonObject.getJSONObject("destMapDef"));
-            header.setDestMapDef(destMapDef);
-        }
-
-
-        if (jsonObject.has("itiType")) {
-            header.setItiType(jsonObject.getLong("itiType"));
-
-        }
-
-        if (jsonObject.has("itidate") && jsonObject.getString("itidate") != null) {
-            header.setItidate(jsonObject.getString("itidate"));
-
-        }
-
-        if (jsonObject.has("idx")) {
-            header.setIdx(jsonObject.getLong("idx"));
-
-        }
-
-        if (jsonObject.has("vehicle")) {
-            header.setVehicle(jsonObject.getLong("vehicle"));
-        }
-
         if (!jsonObject.has("summaries") || jsonObject.getJSONObject("summaries") == null) {
             return null;
         }
@@ -191,38 +122,8 @@ public class ViaMichelinHelper {
         JSONObject summaryJSON = summariesJSON.getJSONObject("summary");
         Summary summary = new Summary();
 
-        if (summaryJSON.has("ecoTaxDist")) {
-            summary.setEcoTaxDist(summaryJSON.getDouble("ecoTaxDist"));
-
-        }
-
-        if (summaryJSON.has("fullMapDef") && summaryJSON.getJSONObject("fullMapDef") != null) {
-            summary.setFullMapDef(this.parseMapDef(summaryJSON.getJSONObject("fullMapDef")));
-        }
-
-        if (summaryJSON.has("motorwayTime")) {
-            summary.setMotorwayTime(summaryJSON.getDouble("motorwayTime"));
-        }
-
-        if (summaryJSON.has("pleasantDist")) {
-            summary.setPleasentDist(summaryJSON.getDouble("pleasantDist"));
-        }
-
         if (summaryJSON.has("totalTime")) {
             summary.setTotalTime(summaryJSON.getDouble("totalTime"));
-        }
-
-        if (summaryJSON.has("pleasantTime")) {
-            summary.setPleasentTime(summaryJSON.getDouble("pleasantTime"));
-        }
-
-        if (summaryJSON.has("index")) {
-            summary.setIndex(summaryJSON.getLong("index"));
-
-        }
-
-        if (summaryJSON.has("drivingTime")) {
-            summary.setDrivingDist(summaryJSON.getDouble("drivingTime"));
         }
 
         // TODO wichtig
@@ -231,149 +132,17 @@ public class ViaMichelinHelper {
         }
         summary.setConsumption(summaryJSON.getDouble("consumption"));
 
-        if (summaryJSON.has("distanceByCountry")) {
-            summary.setDistanceByCountry(summaryJSON.getString("distanceByCountry"));
-        }
-
-        if (summaryJSON.has("CCZCost") && summaryJSON.getJSONObject("CCZCost") != null && this.parseCost(summaryJSON.getJSONObject("CCZCost")) != null) {
-            summary.setCCZCost(this.parseCost(summaryJSON.getJSONObject("CCZCost")));
-        }
-
-        // TODO wichtig
-        if (summaryJSON.has("tollCost") && summaryJSON.getJSONObject("tollCost") != null && this.parseCost(summaryJSON.getJSONObject("tollCost")) != null) {
-            summary.setTollCost(this.parseCost(summaryJSON.getJSONObject("tollCost")));
-        }
-
-        if (summaryJSON.has("ecoTaxRepercussionRate")) {
-            summary.setEcoTaxRepercussionRate(summaryJSON.getDouble("ecoTaxRepercussionRate"));
-        }
-
-        if (summaryJSON.has("avoidClosedRoadUsed")) {
-            summary.setAvoidClosedRoadUsed(summaryJSON.getBoolean("avoidClosedRoadUsed"));
-        }
-
-        if (summaryJSON.has("names") && summaryJSON.getJSONObject("names") != null) {
-            JSONObject namesJSON = summaryJSON.getJSONObject("names");
-
-            if (namesJSON.has("name") && namesJSON.optJSONArray("name") != null) {
-                JSONArray nameJSON = namesJSON.optJSONArray("name");
-                ArrayList<String> name = new ArrayList<>();
-                for (int i = 0; i < nameJSON.length(); i++) {
-                    if (!nameJSON.isNull(i)) {
-                        name.add(nameJSON.getString(i));
-                    }
-                }
-                summary.setNames(name);
-            }
-        }
-        if (summaryJSON.has("eventTrafficDatabaseAvailable")) {
-            summary.setEventTrafficDatabaseAvailable(summaryJSON.getBoolean("eventTrafficDatabaseAvailable"));
-        }
-
-        // TODO wichtig
-        if (!summaryJSON.has("drivingDist")) {
-            return null;
-        }
-        summary.setDrivingDist(summaryJSON.getDouble("drivingDist"));
-
         // TODO wichtig
         if (!summaryJSON.has("totalDist")) {
             return null;
         }
         summary.setTotalDist(summaryJSON.getDouble("totalDist"));
 
-        if (summaryJSON.has("ecoTax")) {
-            summary.setEcoTax(summaryJSON.getDouble("ecoTax"));
-        }
-
-        if (summaryJSON.has("motorwayDist")) {
-            summary.setMotorwayDist(summaryJSON.getDouble("motorwayDist"));
-        }
-
         header.setSummaries(summary);
 
         return header;
     }
-
-    /**
-     * Hilfsmethode zu Parsen des MapDef-Objekts
-     *
-     * @param jsonObject JSONObject
-     * @return MapDef
-     */
-    private MapDef parseMapDef(JSONObject jsonObject) {
-        if (!jsonObject.has("size")) {
-            return null;
-        }
-        JSONObject size = jsonObject.getJSONObject("size");
-        MapDef mapDef = new MapDef();
-
-        if (!jsonObject.has("id") && jsonObject.getString("id") == null) {
-            return null;
-        }
-        mapDef.setId(jsonObject.getString("id"));
-
-        MapSize mapSize = new MapSize();
-        if (!size.has("w")) {
-            return null;
-        }
-        mapSize.setW(size.getLong("w"));
-
-        if (!size.has("h")) {
-            return null;
-        }
-        mapSize.setH(size.getLong("h"));
-        mapDef.setSize(mapSize);
-
-        return mapDef;
-    }
-
-    /**
-     * Hilfsmethode zu Parsen des Cost-Objekts
-     *
-     * @param jsonObject JSONObject
-     * @return Cost
-     */
-    private Cost parseCost(JSONObject jsonObject) {
-        Cost cost = new Cost();
-        if (!jsonObject.has("car")) {
-            return null;
-        }
-        cost.setCar(jsonObject.getDouble("car"));
-
-        if (!jsonObject.has("caravan")) {
-            return null;
-        }
-        cost.setCaravan(jsonObject.getDouble("caravan"));
-
-        if (!jsonObject.has("pl2")) {
-            return null;
-        }
-        cost.setPl2(jsonObject.getDouble("pl2"));
-
-        if (!jsonObject.has("pl3")) {
-            return null;
-        }
-        cost.setPl3(jsonObject.getDouble("pl3"));
-
-        if (!jsonObject.has("pl4")) {
-            return null;
-        }
-        cost.setPl4(jsonObject.getDouble("pl4"));
-
-        if (!jsonObject.has("pl5")) {
-            return null;
-        }
-        cost.setPl5(jsonObject.getDouble("pl5"));
-
-        if (!jsonObject.has("moto")) {
-            return null;
-        }
-        cost.setMoto(jsonObject.getDouble("moto"));
-
-        return cost;
-    }
-
+    
     /**
      * Überprüfen der Koordinaten
      *
